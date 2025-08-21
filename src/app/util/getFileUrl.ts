54f98ce6a724e5eb055c23b66fc9e4bec6cd73ff
seguel <ariel.alejandrocs@gmail.com>
2025-08-20 21:50:05 -0300

    ajustes para carregar arquivos do uploads

--- src/app/util/getFileUrl.ts
+++ src/app/util/getFileUrl.ts
@@ -1,6 +1,6 @@
 // utils/getFileUrl.ts
 export const getFileUrl = (fileName?: string) => {
   if (!fileName) return ""; // caso não tenha arquivo
-  const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "";
+  const BASE_URL = process.env.NEXT_PUBLIC_FILE_BASE_URL || "";
   return `${BASE_URL}/uploads/${fileName}`;
 };
